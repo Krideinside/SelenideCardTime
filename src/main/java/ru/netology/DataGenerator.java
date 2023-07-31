9f8226a39a3eabf374f8be55c26b6077eb2ef202
Aleksandr Trufmanov <kride2009@yandex.ru>
2023-07-31 15:31:55 +0300

    tests added

---
+++ src/main/java/ru/netology/DataGenerator.java
@@ -0,0 +1,21 @@
+package ru.netology;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity() {
+        var cities = new String[]{"Майкоп", "Нальчик", "Москва", "Санкт-Петербург", "Казань",
+                "Воронеж", "Екатеринбург", "Магадан", "Владимир", "Брянск", "Калуга", "Саратов", "Самара"};
+        return cities[new Random().nextInt(cities.length)];
+    }
+}
--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,28 +1,59 @@
 package ru.netology;
 
 import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.SetValueOptions.withText;
 
 
 public class CardDeliveryTest {
 
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
     @Test
     void shouldTestSuccessForm() {
-        open("http://localhost:9999");
-        $("[placeholder='Город']").setValue("Москва");
-        $("[placeholder='Дата встречи']").setValue("27.07.2023");
-        $("[name='name']").setValue("Александр Труфманов");
-        $("[name='phone']").setValue("+79262627815");
+        var daysToAddForMeeting = 4;
+        var meetingDate = DataGenerator.generateDate(daysToAddForMeeting);
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(meetingDate);
+        $("[data-test-id=name] input").setValue("Александр Труфманов");
+        $("[data-test-id=phone] input").setValue("+79262627815");
         $("[data-test-id='agreement']").click();
-        $(".button__text").click();
-        $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
-       // $("[data-test-id='notification']").shouldHave(Condition.exactText("Успешно!"));
+        $(byText("Забронировать")).click();
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id='notification'] .notification__content")
+                .shouldHave(exactText("Встреча успешно забронирована на " + meetingDate))
+                .shouldBe(visible);
+    }
+
+    @Test
+    void shouldTestSuccessFormWithCalendar() {
+        var daysToAddForMeeting = 4;
+        var meetingDate = DataGenerator.generateDate(daysToAddForMeeting);
+        $("[data-test-id=city] input").setValue("Мо");
+        $(byText("Москва")).click();
+        $("[data-test-id=date]").click();
+        $(".popup__content").shouldBe(visible);
+        $(".calendar__arrow calendar__arrow_direction_right").click();
+        $(".calendar__day").shouldHave(text("8")).click();
+        $("[data-test-id=name] input").setValue("Александр Труфманов");
+        $("[data-test-id=phone] input").setValue("+79262627815");
+        $("[data-test-id='agreement']").click();
+        $(byText("Забронировать")).click();
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id='notification'] .notification__content")
+                .shouldHave(exactText("Встреча успешно забронирована на " + meetingDate))
+                .shouldBe(visible);
     }
 }
