4375dbfcb74085a2d5721075cee4532a3704cf38
Aleksandr Trufmanov <kride2009@yandex.ru>
2023-08-05 01:08:42 +0300

    Code fixed

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,22 +1,12 @@
 package ru.netology;
 
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.devtools.v109.css.model.Value;
-
 import java.time.Duration;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.SetValueOptions.withText;
-import static com.codeborne.selenide.files.DownloadActions.click;
-
 
 public class CardDeliveryTest {
 
@@ -56,21 +46,9 @@ public class CardDeliveryTest {
         $("[data-test-id=phone] input").setValue("+79262627815");
         $("[data-test-id='agreement']").click();
         $(byText("Забронировать")).click();
-//        $("[.data-test-id='notification']")
-//        $("[.data-test-id='notification'] .notification__title")
-        $("[data-test-id='notification']")
-                .shouldBe(visible, Duration.ofSeconds(15));
-        $$(".notification__title").findBy(text("Успешно"));
-        $$(".notification__content").findBy(text("Встреча успешно забронирована на " + meetingDate));
-//                .shouldHave(exactText("Встреча успешно забронирована на " + meetingDate));
-//////                .shouldBe(visible);
-//        $("[.data-test-id='notification'] .notification__content")
-//                .shouldHave(exactText("Встреча успешно забронирована на " + meetingDate))
-//                .shouldBe(visible, Duration.ofSeconds(15));
-//                .shou
-//        ).findBy(text("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
-//        $("[data-test-id='notification'] .notification__content")
-//                .shouldHave(exactText("Встреча успешно забронирована на " + meetingDate))
-//                .shouldBe(visible);
+        $("[data-test-id='notification'] .notification__title").shouldBe(visible, Duration.ofSeconds(15))
+                .equals(exactText("Успешно!"));
+        $("[data-test-id='notification'] .notification__content").shouldBe(visible, Duration.ofSeconds(15))
+                .equals(exactText("Встреча успешно забронирована на " + meetingDate));
     }
 }
